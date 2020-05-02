0807bf0610fde041c406ee7e22b8c72fa367d59a
KukuruzaAndrey <andreyku6@gmail.com>
2020-05-03 00:22:46 +0300

    Updates

---
+++ precache-manifest.b29427e5ed7c5a1043ea7aa5206a84df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793b388dddbd2670cf621d4df8cbf5fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8306b340fc852383675",
+    "url": "/static/css/main.b78a20d4.chunk.css"
+  },
+  {
+    "revision": "f651e045f40ce368a1de",
+    "url": "/static/js/2.77b675bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77b675bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8306b340fc852383675",
+    "url": "/static/js/main.f84b4ffe.chunk.js"
+  },
+  {
+    "revision": "639a756cc22eb8605bfd",
+    "url": "/static/js/runtime-main.b0092b6b.js"
+  }
+]);
